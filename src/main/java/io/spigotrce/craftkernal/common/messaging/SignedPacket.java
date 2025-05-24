25ded5b30578dba99c4abf33e3d728c7cd7e5918
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-05-24 17:23:52 +0530

    feat: SignedPacket

---
+++ src/main/java/io/spigotrce/craftkernal/common/messaging/SignedPacket.java
@@ -0,0 +1,28 @@
+package io.spigotrce.craftkernal.common.messaging;
+
+import java.util.UUID;
+
+/**
+ * Abstract class representing a packet that is signed by an uuid.
+ */
+public abstract class SignedPacket implements Packet {
+    private UUID uuid;
+
+    public SignedPacket(UUID playerUuid) {
+        this.uuid = playerUuid;
+    }
+
+    @Override
+    public final void encode(PacketBuffer buffer) {
+        buffer.writeUUID(uuid);
+    }
+
+    @Override
+    public final void decode(PacketBuffer buffer) {
+        this.uuid = buffer.readUUID();
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+}
