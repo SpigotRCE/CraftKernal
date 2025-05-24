bb063cf1152c25220b4cd09783a0fb543872b5dc
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-05-24 16:53:36 +0530

    feat: BungeeCommand

---
+++ src/main/java/io/spigotrce/craftkernal/bungee/command/BungeeCommand.java
@@ -0,0 +1,45 @@
+package io.spigotrce.craftkernal.bungee.command;
+
+import io.spigotrce.craftkernal.bungee.BungeeHolder;
+import io.spigotrce.craftkernal.common.holder.command.CommandHolder;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
+
+public abstract class BungeeCommand extends CommandHolder implements TabExecutor {
+    /**
+     * The {@link BungeeHolder} holder.
+     */
+    private final BungeeHolder holder;
+
+    /**
+     * Constructs a new BungeeCommandHolder with the specified commandName server, permissionNode, and commandAliases.
+     *
+     * @param holder         The BungeeHolder instance.
+     * @param commandName    The name of the command.
+     * @param permissionNode The top level permission node for the command.
+     * @param commandAliases The aliases of the command.
+     */
+    public BungeeCommand(BungeeHolder holder, String commandName, String permissionNode, String... commandAliases) {
+        super(commandName, permissionNode, commandAliases);
+        this.holder = holder;
+
+        new Command(getCommandName(), getPermissionNode(), getCommandAliases()) {
+            @Override
+            public void execute(CommandSender sender, String[] args) {
+                BungeeCommand.this.execute(sender, args);
+            }
+        };
+    }
+
+    /**
+     * Gets the {@link BungeeHolder} holder.
+     *
+     * @return The {@link BungeeHolder} holder.
+     */
+    public BungeeHolder getHolder() {
+        return holder;
+    }
+
+    public abstract void execute(CommandSender sender, String[] args);
+}
